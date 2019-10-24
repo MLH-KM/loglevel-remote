4c47aec3ac96357bc300b603a0f1f0d5c9ad25ad
Caleb Williams <br0p0p@users.noreply.github.com>
2019-10-24 16:26:52 -0500

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,15 @@ module.exports = {
     parser: 'babel-eslint',
     extends: [
         'eslint:recommended',
+
+        'plugin:import/errors',
+        'plugin:import/warnings',
+
         'plugin:prettier/recommended',
-        'plugin:promise/recommended'
+
+        'plugin:promise/recommended',
+
+        'plugin:unicorn/recommended'
     ],
     env: {
         browser: true,
@@ -15,5 +22,9 @@ module.exports = {
     },
     globals: {
         __VERSION__: true
+    },
+    rules: {
+        'unicorn/prevent-abbreviations': 0,
+        'unicorn/no-nested-ternary': 0
     }
 };
